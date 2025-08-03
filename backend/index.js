bb9fedcd0614747704ad1dbd1d2e6d95f5feb932
Chad <chaddiao0@gmail.com>
2025-08-03 03:36:30 -0500

    index

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api', GVRoutes);
 app.use('/groq', groqRoutes);
 app.use('/api/scores', scoreRoutes);
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
